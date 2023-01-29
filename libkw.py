354a54fcc1d15910a6db01c0134173613a4c71ef
Marcin Matula <marcin.matula@gmail.com>
2023-01-29 22:40:39 +0100

    libkw - handle_kwargs - can return alternative found in kwargs

--- libkw.py
+++ libkw.py
@@ -1,4 +1,4 @@
-def handle_kwargs(keys, default_output = None, is_required = False, **kwargs):
+def handle_kwargs(keys, default_output = None, is_required = False, return_alternative = False, **kwargs):
     if isinstance(keys, str):
         return handle_kwargs([keys], default_output = default_output, is_required = is_required, **kwargs)
     keys_in_kwargs = [k for k in keys if k in kwargs]
@@ -7,6 +7,9 @@ def handle_kwargs(keys, default_output = None, is_required = False, **kwargs):
     if len(keys_in_kwargs) == 0:
         if is_required:
             raise Exception(f"Arg {keys} is required")
+        if len(keys) > 1 and return_alternative:
+            return default_output, None
         return default_output
+    if len(keys) > 1 and return_alternative:
+        return kwargs[keys_in_kwargs[0]], keys_in_kwargs[0]
     return kwargs[keys_in_kwargs[0]]
-
