65596500b4d1f5b913943aeca28e300186e886d1
Marcin Matula <marcin.matula@gmail.com>
2023-10-13 18:31:51 +0200

    libgrep.py - refactoring

--- libgrep.py
+++ libgrep.py
@@ -9,51 +9,12 @@ import logging
 from pylibcommons import libkw
 from pylibcommons.private.libtempfile import create_temp_file
 
+__logger = logging.getLogger(__name__)
+
 def grep(filepath, regex, **kwargs):
-    fromLine = libkw.handle_kwargs(["fromLine", "from_line", "n"], default_output = 1, **kwargs)
-    maxCount = libkw.handle_kwargs(["maxCount", "max_count", "m"], default_output = -1, **kwargs)
-    onlyMatch = libkw.handle_kwargs(["onlyMatch", "only_match", "o"], default_output = False, **kwargs)
-    if fromLine < 1:
-        raise Exception(f"Invalid fromLine value {fromLine}")
-    if maxCount < -1:
-        raise Exception(f"Invalid value of maxCount {maxCount}. It should be > -1")
-    lineNumber = True #hardcode
-    def makeArgs(lineNumber, maxCount):
-        o_arg = " -o" if onlyMatch else ""
-        n_arg = " -n" if lineNumber else ""
-        m_arg = " -m {maxCount}" if maxCount > -1 else ""
-        return f"{o_arg}{n_arg}{m_arg}"
-    import subprocess, os
-    args = makeArgs(lineNumber, maxCount)
-    command = f"grep -a {args} -e \"{regex}\""
-    if fromLine > 1:
-        command = f"sed -n '{fromLine},$p' {filepath} | {command}"
-    else:
-        command = f"{command} {filepath}"
-    if command == None:
-        raise Exception("Grep command was failed on initialization")
-    logging.debug(f"{grep.__name__}: {command}")
-    with create_temp_file() as fout, create_temp_file() as ferr:
-        logging.debug(f"{grep.__name__}: fout {fout.name}")
-        logging.debug(f"{grep.__name__}: ferr {ferr.name}")
-        def readlines(f):
-            lines = f.readlines()
-            line_offset = 0 if fromLine < 1 else fromLine - 1
-            if not lineNumber:
-                lines = [GrepOutput(matched = l, line_offset = line_offset) for l in lines]
-            else:
-                lines = [GrepOutput.from_split(l, line_offset = line_offset) for l in lines]
-            return lines
-        process = subprocess.Popen(command, shell=True, stdout=fout, stderr=ferr)
-        process.wait()
-        if os.path.getsize(ferr.name) > 0:
-            ferr.seek(0)
-            err = readlines(ferr)
-            remove()
-            raise Exception(err)
-        fout.seek(0)
-        out = readlines(fout)
-        return out
+    support_directory = libkw.handle_kwargs(["support_directory", "supportDirectory"], False, **kwargs)
+    __handle_directory()
+    __grep(filepath, regex, **kwargs)
 
 def grep_in_text(text, regex, **kwargs):
     try:
@@ -90,7 +51,7 @@ class GrepOutput:
                 self.line_number = int(line_number.rstrip())
                 self.line_number = self.line_number + line_offset
             except Exception as ex:
-                logging.error(f"{line_number} cannot be converted to int! ${ex}")
+                __logger.error(f"{line_number} cannot be converted to int! ${ex}")
                 raise ex
         self.matched = matched.rstrip()
     def __getitem__(self, idx):
@@ -108,3 +69,49 @@ class GrepOutput:
     def from_split(line, line_offset = 0):
         out = line.split(':', 1)
         return GrepOutput(out[0], out[1], line_offset)
+
+def __grep(filepath, regex, **kwargs):
+    fromLine = libkw.handle_kwargs(["fromLine", "from_line", "n"], default_output = 1, **kwargs)
+    maxCount = libkw.handle_kwargs(["maxCount", "max_count", "m"], default_output = -1, **kwargs)
+    onlyMatch = libkw.handle_kwargs(["onlyMatch", "only_match", "o"], default_output = False, **kwargs)
+    if fromLine < 1:
+        raise Exception(f"Invalid fromLine value {fromLine}")
+    if maxCount < -1:
+        raise Exception(f"Invalid value of maxCount {maxCount}. It should be > -1")
+    lineNumber = True #hardcode
+    def makeArgs(lineNumber, maxCount):
+        o_arg = " -o" if onlyMatch else ""
+        n_arg = " -n" if lineNumber else ""
+        m_arg = " -m {maxCount}" if maxCount > -1 else ""
+        return f"{o_arg}{n_arg}{m_arg}"
+    import subprocess, os
+    args = makeArgs(lineNumber, maxCount)
+    command = f"grep -a {args} -e \"{regex}\""
+    if fromLine > 1:
+        command = f"sed -n '{fromLine},$p' {filepath} | {command}"
+    else:
+        command = f"{command} {filepath}"
+    if command == None:
+        raise Exception("Grep command was failed on initialization")
+    __logger.debug(f"{grep.__name__}: {command}")
+    with create_temp_file() as fout, create_temp_file() as ferr:
+        __logger.debug(f"{grep.__name__}: fout {fout.name}")
+        __logger.debug(f"{grep.__name__}: ferr {ferr.name}")
+        def readlines(f):
+            lines = f.readlines()
+            line_offset = 0 if fromLine < 1 else fromLine - 1
+            if not lineNumber:
+                lines = [GrepOutput(matched = l, line_offset = line_offset) for l in lines]
+            else:
+                lines = [GrepOutput.from_split(l, line_offset = line_offset) for l in lines]
+            return lines
+        process = subprocess.Popen(command, shell=True, stdout=fout, stderr=ferr)
+        process.wait()
+        if os.path.getsize(ferr.name) > 0:
+            ferr.seek(0)
+            err = readlines(ferr)
+            remove()
+            raise Exception(err)
+        fout.seek(0)
+        out = readlines(fout)
+        return out
