867dbe606595147babdc137a8076549705ffb1a8
Marcin Matula <marcin.matula@gmail.com>
2025-06-20 13:27:19 +0200

    libloop.py - while_with_timeout impl

---
+++ uts/ut_libloop.py
@@ -0,0 +1,21 @@
+__author__ = "Marcin Matula"
+__copyright__ = "Copyright (C) 2022, Marcin Matula"
+__credits__ = ["Marcin Matula"]
+__license__ = "Apache License"
+__version__ = "2.0"
+__maintainer__ = "Marcin Matula"
+
+from unittest import mock
+import pytest
+
+@mock.patch("time.sleep")
+@mock.patch("time.time")
+def test_while_with_timeout(time_time_mock, time_sleep_mock):
+    from pylibcommons import libloop
+    with pytest.raises(libloop.TimeoutException) as excinfo:
+        time_sleep_mock.side_effect = lambda x: print(f"Sleeping for {x} seconds")
+        time_time_mock.side_effect = [0, 0, 0.7, 1] 
+        def condition():
+            return True
+        libloop.while_with_timeout(timeout = 1, condition = lambda: True, time_sleep = 0.7)
+        time_sleep_mock.assert_has_calls([call(0.7), call(0.3)])
